0109ad80899cac5191bbf3df01ae4a91b647b90f
Bird <mrpblg@inf.elte.hu>
2021-09-10 18:02:38 +0200

    Commit first exercise

---
+++ FirstExercise/index.js
@@ -0,0 +1,23 @@
+//1.
+console.log("Hello world!");
+//2.
+/**
+ *
+ * @param {number}fahrenheit
+ * @returns {number}
+ */
+
+function fahrenheit_to_Celsius(fahrenheit) {
+    return (fahrenheit-32)/1.8;
+}
+console.log(fahrenheit_to_Celsius(55));
+
+//3.
+/**
+ *
+ * @param {number}number
+ * @param {number}percent
+ */
+function calculatePercent(number,percent) {
+    return
+}
