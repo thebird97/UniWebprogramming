cca3e82256d15e98cb2e5cfe40519f3a3f7702ef
Bird <mrpblg@inf.elte.hu>
2022-01-06 14:49:22 +0100

    task3 ready

--- JavaScript javító évfolyam ZH minta (2020-21-1)/2/index.js
+++ JavaScript javító évfolyam ZH minta (2020-21-1)/2/index.js
@@ -5,3 +5,78 @@ const border = document.querySelector('#border')
 const div = document.querySelector('#images')
 const bigImage = document.querySelector('#big')
 
+
+function copyto() {
+    let mytext = textarea.value;
+    let myarr = mytext.split('\n');
+    console.log(myarr);
+
+
+    // a. (2 pont) A button-to-select gombra kattintva másold át a szöveges beviteli mező sorait a listába!
+    for (let i = 0; i < myarr.length; i++) {
+        let li = document.createElement("option");
+        li.appendChild(document.createTextNode(myarr[i]));
+        select.appendChild(li);
+    }
+
+
+//b. (2 pont) A listaelemben egy kép URL-t kiválasztva, az megjelenik az images azonosítójú div-en belül képként.
+
+
+}
+
+button.addEventListener('click', copyto)
+
+function getImage() {
+
+    //c. (1 pont) A listaelemben több kép URL-t választva (Ctrl billentyű segítségével) mindegyik kiválasztott kép megjelenik az images azonosítójú div-en belül.
+    document.body.onclick = function (e) {
+        if (e.ctrlKey) {
+            /* let imgarray = [];
+             imgarray.push(select.value);
+            // console.log("imgarrta" +  imgarray);
+
+
+             for (let i = 0; i < imgarray.length; i++) {
+                 console.log("aarrayfutle" + imgarray[i])
+                 let img = document.createElement("img");
+                 img.src = imgarray[i];
+                 div.appendChild(img);
+
+
+             }
+         */
+            const options = Array.from(select.selectedOptions)
+            div.innerHTML = ''
+            options.forEach(option => div.innerHTML += `<img src="${option.text}">`)
+        } else {
+            //b. (2 pont) A listaelemben egy kép URL-t kiválasztva, az megjelenik az images azonosítójú div-en belül képként.
+            div.innerHTML = "";
+            let img = document.createElement("img");
+            img.src = select.value;
+            div.appendChild(img);
+        }
+    }
+
+
+}
+
+select.addEventListener('click', getImage);
+
+
+//d. (1 pont) Egy kis kép fölé víve az egeret az adott kép megjelenik nagy képként a big azonosítójú képben.
+function rajtaVan(e) {
+    if(e.target.matches('img')){
+        bigImage.src = e.target.src;
+    }
+}
+
+div.addEventListener('mouseover',rajtaVan);
+
+//e. (2 pont) A border azonosítójú range slidert mozgatva a big azonosítójú kép keretének vastagsága a kiválasztott értéknek megfelelően változik.
+
+function updateBorder() {
+    bigImage.style.borderWidth = parseInt(border.value) + 'px';
+}
+
+border.addEventListener('input',updateBorder);
--- JavaScript javító évfolyam ZH minta (2020-21-1)/3/script.js
+++ JavaScript javító évfolyam ZH minta (2020-21-1)/3/script.js
@@ -1,11 +1,125 @@
 const canvas  = document.querySelector("canvas")
 const ctx     = canvas.getContext("2d")
+const plusButton = document.querySelector("#plus")
+const minusButton = document.querySelector("#minus")
+const nextButton = document.querySelector("#next");
+const simulateButton = document.querySelector("#sim");
+
+
+
+
+
+//a. (1 pont) Állíts elő egy 7 hosszú tömböt 50 és 350 közötti véletlenszámokkal, ami az új fertőzöttek számát tartalmazza az előző hét napban! A tömbben tárolt értékeket írasd ki a konzolra is!
+
+const arry_length = 7
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let numberofInfected = [];
+for (let i = 0; i < arry_length; i++) {
+    numberofInfected.push(getRandomInt(50,350));
+}
+
+console.log(numberofInfected);
+
+numberofInfected[7] = numberofInfected[6];
+
+
+
+
+//e. (1 pont) A + és - feliratú gombokra kattintva az aktuális napi fertőzöttek száma növekedjen vagy csökkenjen 10 fővel, és a változás jelenjen meg azonnal a grafikonon!
+function plusClick() {
+    numberofInfected[7] +=10;
+    draw()
+}
+plusButton.addEventListener('click',plusClick)
+
+
+function minusClick() {
+    numberofInfected[7] -=10;
+    draw()
+}
+minusButton.addEventListener('click',minusClick)
+
+
+
+///f. (1 pont) A Next feliratú gombra kattintva a következő napra léptetjük a bevitelt, tehát a grafikon egy nappal balra tolódik.
+function rotateLeft(arr){
+    let first = arr.shift();
+    arr.push(first);
+    return arr;
+}
+
+function nextClick() {
+    rotateLeft(numberofInfected);
+    draw()
+}
+
+
+nextButton.addEventListener('click',nextClick)
+
+
+//g. (1 pont) A Simulate feliratú gombra kattintva bizonyos időközönként (pl. fél másodpercenként) automatikusan kerüljön a grafikonra egy új nap,
+// ahol a fertőzöttek száma az előző napi érték fele! Ezzel a járvány exponenciális lecsengését szimuláljuk.
+
+
+
+function simulateClick() {
+    setInterval(function(){
+        numberofInfected.shift()
+        numberofInfected[7] = 0.5 * numberofInfected[6]
+        draw()
+    }, 500)
+
+
+
+}
+
+simulateButton.addEventListener('click',simulateClick)
+
 
 function draw(){
     ctx.drawImage(bg, 0, 0)
-    // TODO
+
+
+
+    /*
+b. (2 pont) Jelenítsd meg a tömbben tárolt adatokat grafikusan a mintán látható módon!
+A rajz akkor fog a háttérre illeszkedni, ha az első szakasz kezdőpontjának x-koordinátája 100, a másodiké 200, stb.
+ Az y-koordinátát a tömbben tárolt értékek határozzák meg, de ügyelj a koordinátarendszer állására és a háttérképen feltüntetett feliratokra!
+
+*/
+    for (let i = 0; i < 7; i++){
+        ctx.beginPath()
+        ctx.moveTo(100 * i + 100, canvas.height - numberofInfected[i])
+        ctx.lineTo(100 * (i+1) + 100, canvas.height - numberofInfected[i+1])
+        ctx.strokeStyle = i < 6 ? "blue" : "red"
+        ctx.lineWidth = 3
+        ctx.stroke()
+    }
+
+
+
+
+
 }
 
 let bg        = new Image()
 bg.src        = "bg.png"
 bg.onload     = draw
+
+
+
+
+
+
+
+
+
+
+
+
+
